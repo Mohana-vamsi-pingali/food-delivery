0aafa921dcea2840bb6c627d7e4d79502ec34009
Mohana-vamsi-pingali <vamsipingali2001@gmail.com>
2025-09-02 11:09:48 -0500

    Added Backend Directory

---
+++ back-end/DishPatch/src/main/java/com/food_delivery/DishPatch/DishPatchApplication.java
@@ -0,0 +1,13 @@
+package com.food_delivery.DishPatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishPatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishPatchApplication.class, args);
+	}
+
+}
---
+++ back-end/DishPatch/src/test/java/com/food_delivery/DishPatch/DishPatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_delivery.DishPatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishPatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
