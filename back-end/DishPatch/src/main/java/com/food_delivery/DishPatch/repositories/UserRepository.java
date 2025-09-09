014903e9c6d8987ee10d7b983c8202fccdac082b
Mohana-vamsi-pingali <vamsipingali2001@gmail.com>
2025-09-09 18:38:34 -0500

    Changed User Repository

--- back-end/DishPatch/src/main/java/com/food_delivery/DishPatch/repositories/UserRepository.java
+++ back-end/DishPatch/src/main/java/com/food_delivery/DishPatch/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.food_delivery.DishPatch.repositories;
 
 import com.food_delivery.DishPatch.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByEmail(String email);
 }
